3d5403895241d9e62e51ec1796c47bad0d720e8a
ana-ort28 <ana.alv.ort.28@gmail.com>
2024-10-13 13:15:04 -0600

    Delete assets/jp directory

--- assets/jp/app.js
+++
@@ -1,9 +0,0 @@
-const scrollUP = document.querySelector("#scroll-up");
-
-scrollUP.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        left: 0, // Cambia '=' por ':' aquí
-        behavior: "smooth",
-    });
-});
